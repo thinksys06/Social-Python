51e382161f7337f63ea7ed485bdc4fed538e1b28
Atul Prakash <atul@rhythmscienceinc.com>
2022-03-25 13:26:39 +0530

    now i am coming from the bugfix branch

--- socialPython_app/serializers.py
+++ socialPython_app/serializers.py
@@ -1,17 +1,23 @@
-
-from pyexpat import model
 from rest_framework import serializers
 from socialPython_app.models import userModel, userProfileModel
+import re
 
 class userProfileSerializer(serializers.ModelSerializer):
     class Meta:
         model= userProfileModel
         fields= '__all__'
 
+    def validate_gender(self, gender):
+        if (re.match(" 'male' | 'female' ", gender) == None):
+            raise serializers.ValidationError("gender accepts only numeric values")
+        return gender
+
 class userSerializer(serializers.ModelSerializer):
     relation = userProfileSerializer(read_only= True, many= True)
     class Meta:
         model = userModel
         fields= '__all__'
+
+    
 
 
